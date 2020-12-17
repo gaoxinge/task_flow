35dfe379f4c3257f869699d820cc489ff2fc5519
xg.gao <xg.gao@tianrang-inc.com>
2020-12-17 11:06:49 +0800

    executor

--- README.md
+++ README.md
@@ -19,8 +19,7 @@ pip install git+https://github.com/gaoxinge/task_flow
 ```python
 from task_flow import Graph, Task
 
-
-with Graph("test") as graph:
+with Graph(name="test") as graph:
     _int1 = Task("int1", lambda: 1)
     _int2 = Task("int2", lambda: 2)
     _add = Task("add", lambda x, y: x + y, _int1, _int2)
@@ -36,9 +35,8 @@ with Graph("test") as graph:
 ```python
 from task_flow import Graph, Task, SimpleExecutor
 
-
 with SimpleExecutor() as executor:
-    with Graph("test") as graph:
+    with Graph(name="test") as graph:
         _int1 = Task("int1", lambda: 1)
         _int2 = Task("int2", lambda: 2)
         _add = Task("add", lambda x, y: x + y, _int1, _int2)
@@ -50,3 +48,41 @@ with SimpleExecutor() as executor:
 ```
 
 ### run task graph by thread executor
+
+```python
+from task_flow import Graph, Task, ThreadExecutor
+
+with ThreadExecutor(num=3) as executor:
+    with Graph(name="test") as graph:
+        _int1 = Task("int1", lambda: 1)
+        _int2 = Task("int2", lambda: 2)
+        _add = Task("add", lambda x, y: x + y, _int1, _int2)
+        _minus = Task("minus", lambda x, y: x - y, _int1, _int2)
+        _multiply = Task("multiply", lambda x, y: x * y, _int1, _int2)
+        _divide = Task("divide", lambda x, y: x // y, _int1, _int2)
+        _print = Task("print", lambda x, y, z, w: print(x, y, z, w), _add, _minus, _multiply, _divide)
+        executor.run(graph)
+```
+
+### run task graph by process executor
+
+```python
+from task_flow import Graph, Task, ProcessExecutor
+
+with ProcessExecutor(num=3) as executor:
+    with Graph(name="test") as graph:
+        # pickle and unpickle are used in multiprocess, but
+        # can not be used for lambda function in windows
+        def int1(): return 1
+        def int2(): return 2
+        from operator import add, sub, mul, floordiv
+
+        _int1 = Task("int1", int1)
+        _int2 = Task("int2", int2)
+        _add = Task("add", add, _int1, _int2)
+        _minus = Task("minus", sub, _int1, _int2)
+        _multiply = Task("multiply", mul, _int1, _int2)
+        _divide = Task("divide", floordiv, _int1, _int2)
+        _print = Task("print", print, _add, _minus, _multiply, _divide)
+        executor.run(graph)
+```
--- task_flow/executor.py
+++ task_flow/executor.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
+from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor, wait, FIRST_COMPLETED
 
-__all__ = ["Executor", "SimpleExecutor"]
+__all__ = ["Executor", "SimpleExecutor", "ThreadExecutor", "ProcessExecutor"]
 
 
 class Executor(ABC):
@@ -38,3 +39,65 @@ class SimpleExecutor(Executor):
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         pass
+
+
+class ThreadExecutor(Executor):
+
+    def __init__(self, num):
+        self.thread_pool = ThreadPoolExecutor(max_workers=num)
+
+    def run(self, graph):
+        _stack = [root for root in graph.roots]
+        _futures = [self.thread_pool.submit(root.f) for root in graph.roots]
+        _degree = {task.name: len(task.parents) for task in graph if task not in graph.roots}
+        _output = {}
+        while len(_stack) != 0:
+            wait(_futures, return_when=FIRST_COMPLETED)
+            i = next(_ for _, _future in enumerate(_futures) if _future.done())
+            task = _stack.pop(i)
+            _future = _futures.pop(i)
+            _output[task.name] = _future.result()
+
+            for child in task.children:
+                _degree[child.name] -= 1
+                if _degree[child.name] == 0:
+                    _inputs = [_output[parent.name] for parent in child.parents]
+                    _stack.append(child)
+                    _futures.append(self.thread_pool.submit(child.f, *_inputs))
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.thread_pool.__exit__(exc_type, exc_val, exc_tb)
+
+
+class ProcessExecutor(Executor):
+
+    def __init__(self, num):
+        self.process_pool = ProcessPoolExecutor(max_workers=num)
+
+    def run(self, graph):
+        _stack = [root for root in graph.roots]
+        _futures = [self.process_pool.submit(root.f) for root in graph.roots]
+        _degree = {task.name: len(task.parents) for task in graph if task not in graph.roots}
+        _output = {}
+        while len(_stack) != 0:
+            wait(_futures, return_when=FIRST_COMPLETED)
+            i = next(_ for _, _future in enumerate(_futures) if _future.done())
+            task = _stack.pop(i)
+            _future = _futures.pop(i)
+            _output[task.name] = _future.result()
+
+            for child in task.children:
+                _degree[child.name] -= 1
+                if _degree[child.name] == 0:
+                    _inputs = [_output[parent.name] for parent in child.parents]
+                    _stack.append(child)
+                    _futures.append(self.process_pool.submit(child.f, *_inputs))
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.process_pool.__exit__(exc_type, exc_val, exc_tb)
