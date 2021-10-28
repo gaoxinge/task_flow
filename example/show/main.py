10e909bea9f37ae96bfe63b3c14cda1e43e44f67
gaoxinge <gaoxx5@gmail.com>
2021-10-29 00:28:50 +0800

    example

---
+++ example/show/main.py
@@ -0,0 +1,13 @@
+from operator import add, sub, mul, floordiv
+from task_flow import InputTask, Task, Graph
+
+if __name__ == "__main__":
+    with Graph(name="test") as graph:
+        _int1 = InputTask("int1", int)
+        _int2 = InputTask("int2", int)
+        _add = Task("add", add, _int1, _int2, output=True)
+        _sub = Task("sub", sub, _int1, _int2, output=True)
+        _mul = Task("mul", mul, _int1, _int2, execute="process")
+        _div = Task("div", floordiv, _int1, _int2, execute="process")
+        _print = Task("print", print, _add, _sub, _mul, _div)
+        graph.show("result/test.gv")
